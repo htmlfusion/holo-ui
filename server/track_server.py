5719ffb5ed673f3ce931b084743ffb1b75d18236
Jesse Vander Does <jesse@htmlfusion.com>
2015-02-07 13:48:23 -0800

    Adding python websocket server

---
+++ server/track_server.py
@@ -0,0 +1,30 @@
+import cherrypy
+from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
+from ws4py.websocket import EchoWebSocket
+from ws4py.websocket import WebSocket
+
+cherrypy.config.update({'server.socket_port': 9001})
+WebSocketPlugin(cherrypy.engine).subscribe()
+cherrypy.tools.websocket = WebSocketTool()
+
+class TrackSocket(WebSocket):
+    
+    def received_message(self, message):
+        if(message.data == 'subscribe'):
+            print('sending')
+            for i in range(10):
+                self.send(str(i), message.is_binary)
+
+class Root(object):
+    @cherrypy.expose
+    def index(self):
+        return 'some HTML with a websocket javascript connection'
+
+    @cherrypy.expose
+    def ws(self):
+        # you can access the class instance through
+        handler = cherrypy.request.ws_handler
+
+cherrypy.quickstart(Root(), '/', config={'/ws': {'tools.websocket.on': True,
+                                                 'tools.websocket.handler_cls': TrackSocket}})
+
