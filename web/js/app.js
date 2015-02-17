3803a0a84f3fc0295dc82586d59a2c263f8e65b8
Jesse Vander Does <jesse@htmlfusion.com>
2015-02-17 01:12:24 -0800

    Metaio tracking is working pretty well, trying to track down render issues

--- web/js/app.js
+++ web/js/app.js
@@ -19,13 +19,20 @@ var APP = {
     var ws = new WebSocket('ws://localhost:8887');
     
     ws.onopen = function(){
+      ws.send('subscribe');
       console.log('socket connected');
     };
     
+    var last = null;
     ws.onmessage = function (evt) { 
-      var position = evt.data.split(',');
+      //var position = evt.data.split(',');
+      var data = JSON.parse(evt.data);
       if(stereoCamera){
-        stereoCamera.setPosition(position);
+        var pos = [data.x, data.y, data.z];
+        if(last && pos[0] !== last[0] && pos[1] !== last[1] && pos[2] !== last[2]){
+          stereoCamera.setPosition(pos);
+        }
+        last = pos;
       }
     };
     
@@ -79,15 +86,16 @@ var APP = {
 
     var animate = function (time) {
 
-      request = requestAnimationFrame(animate);
-
-      if(debugScene.on){
-        debugScene.update();
-        stereoCamera.updateHelpers();
-        stereoCamera.updateFrustum({width: 100, height: 75});
-      } else {
-        stereoCamera.render({width: 100, height: 75});
-      }
+      setTimeout(function() {
+        request = requestAnimationFrame(animate);
+        if(debugScene.on){
+          debugScene.update();
+          stereoCamera.updateHelpers();
+          stereoCamera.updateFrustum({width: 100, height: 75});
+        } else {
+          stereoCamera.render({width: 100, height: 75});
+        }
+      }, 1000/15);
 
     };
 
