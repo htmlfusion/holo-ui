8149048dfd79dc9ffa79d9a8196b5b3fc9832904
Philippe Lewicki <philippe@htmlfusion.com>
2015-02-17 20:17:49 -0800

    added hands data to the kinnect and now return a json object updated app.js to capture the json data and the hands. #15361#

---
+++ web/js/Hands.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+function LeftHand(scene){
+
+  var self=this;
+  
+  var renderer = renderer;
+  var scene = scene;
+  var group = group;
+  
+  var left = scene.getObjectByName('leftHand');
+
+
+  self.setPosition = function(position){
+    left.position.x = -position[0]/10;
+    left.position.y = position[1]/10;
+    left.position.z = position[2]/10;
+  };
+
+
+}
+
+
+function RightHand(scene){
+
+  var self=this;
+  
+  var renderer = renderer;
+  var scene = scene;
+  var group = group;
+  
+  var right = scene.getObjectByName('rightHand');
+
+
+  self.setPosition = function(position){
+    right.position.x = -position[0]/10;
+    right.position.y = position[1]/10;
+    right.position.z = position[2]/10;
+  };
+
+
+}
