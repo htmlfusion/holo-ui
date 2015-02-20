43570ae7f951112843c28ca9c884a7f20f72952c
Jesse Vander Does <jesse@htmlfusion.com>
2015-02-19 19:10:29 -0800

    Hands and head now working

--- web/js/Hands.js
+++ web/js/Hands.js
@@ -9,15 +9,13 @@ function LeftHand(scene){
   var scene = scene;
   var group = group;
   
-  var left = scene.getObjectByName('leftHand');
-
+  var left = scene.getObjectByName('LeftHand');
 
   self.setPosition = function(position){
     left.position.x = -position[0]/10;
     left.position.y = position[1]/10;
     left.position.z = position[2]/10;
   };
-
 
 }
 
@@ -30,7 +28,7 @@ function RightHand(scene){
   var scene = scene;
   var group = group;
   
-  var right = scene.getObjectByName('rightHand');
+  var right = scene.getObjectByName('RightHand');
 
 
   self.setPosition = function(position){
--- web/js/app.js
+++ web/js/app.js
@@ -2,11 +2,11 @@
 
 var APP = {
 
-  Player: function () {
+  Player: function() {
 
     var loader = new THREE.ObjectLoader();
-    var camera, scene, renderer, debugScene, 
-        stereoCamera, debugCam, hands, leftHand, rightHand; 
+    var camera, scene, renderer, debugScene,
+      stereoCamera, debugCam, hands, leftHand, rightHand;
     var debug = false;
 
     var scripts = {};
@@ -15,62 +15,59 @@ var APP = {
 
     this.width = 500;
     this.height = 500;
-    
+
     var ws = new WebSocket('ws://localhost:8887');
-    
-    ws.onopen = function(){
+
+    ws.onopen = function() {
       ws.send('subscribe');
       console.log('socket connected');
     };
-    
+
     var last = null;
-    ws.onmessage = function (evt) { 
+    ws.onmessage = function(evt) {
       //var position = evt.data.split(',');
-      var bodyPos = JSON.parse(evt.data);
-      if(stereoCamera){
-        var pos = [bodyPos.head.x, bodyPos.head.y, bodyPos.head.z];
-        // if(last && pos[0] !== last[0] && pos[1] !== last[1] && pos[2] !== last[2]){
-        //   stereoCamera.setPosition(pos);
-        // }
-        last = pos;
+      try {
+        var bodyPos = JSON.parse(evt.data);
+      } catch (err) {
+        console.log('error', err);
+        return;
       }
-      if(bodyPos.left_hand){
+
+      if (stereoCamera) {
+        var pos = [bodyPos.head.x, bodyPos.head.y, bodyPos.head.z];
+        stereoCamera.setPosition(pos);
+      }
+      if (bodyPos.left_hand) {
         var left_pos = [bodyPos.left_hand.x, bodyPos.left_hand.y, bodyPos.left_hand.z];
-        if(left_last && left_pos[0] !== left_last[0] && left_pos[1] !== left_last[1] && left_pos[2] !== left_last[2]){
-          leftHand.setPosition(left_pos);
-        }
-        left_last = left_pos;
-	  }
-      if(bodyPos.right_hand){  
+        leftHand.setPosition(left_pos);
+      }
+      if (bodyPos.right_hand) {
         var right_pos = [bodyPos.right_hand.x, bodyPos.right_hand.y, bodyPos.right_hand.z];
-        if(right_last && right_pos[0] !== right_last[0] && right_pos[1] !== right_last[1] && right_pos[2] !== right_last[2]){
-          rightHand.setPosition(right_pos);
-        }
-        right_last = right_pos;
+        rightHand.setPosition(right_pos);
       }
     };
-    
-    ws.onclose = function() { 
+
+    ws.onclose = function() {
       console.log('disconnected')
     };
-    
-    
+
+
     function keydown(event) {
       // <d> key for debug cam
       if (event.keyCode === 100) {
         debugScene.debug(true);
         stereoCamera.debug(true);
-      // <r> key for render cam
+        // <r> key for render cam
       } else if (event.keyCode === 114) {
         debugScene.debug(false);
         stereoCamera.debug(false);
       }
     }
     $(document).keypress(keydown);
-    
-    this.load = function (json) {
 
-      
+    this.load = function(json) {
+
+
       renderer = new THREE.WebGLRenderer({
         antialias: true
       });
@@ -82,57 +79,56 @@ var APP = {
       var group = scene.getObjectByName('cameraGroup');
 
       this.dom = renderer.domElement;
-      
+
       stereoCamera = new StereoCamera(renderer, scene, group);
       debugScene = new DebugScene(renderer, scene, camera);
       //hands = new Hands(scene);
       leftHand = new LeftHand(scene);
       rightHand = new RightHand(scene);
-      
+
       debugScene.debug(false);
       stereoCamera.debug(false);
-      
+
     };
 
 
-    this.setSize = function (width, height) {
+    this.setSize = function(width, height) {
 
       this.width = width;
       this.height = height;
       renderer.setSize(width, height);
-      
+
     };
 
     var request;
 
-    var animate = function (time) {
+    var animate = function(time) {
 
       setTimeout(function() {
         request = requestAnimationFrame(animate);
-        if(debugScene.on){
+        if (debugScene.on) {
           debugScene.update();
-          if(last){
-            stereoCamera.setPosition(last);
-          }
-          stereoCamera.updateFrustum({width: 100, height: 75});
+          stereoCamera.updateFrustum({
+            width: 100,
+            height: 75
+          });
           stereoCamera.updateHelpers();
         } else {
-          if(last){
-            stereoCamera.setPosition(last);
-          }
-          stereoCamera.render({width: 100, height: 75});
+          stereoCamera.render({
+            width: 100,
+            height: 75
+          });
         }
-      }, 1000/60);
+      }, 1000 / 60);
 
     };
 
-    this.play = function () {
+    this.play = function() {
       request = requestAnimationFrame(animate);
     };
 
-    this.stop = function () {
+    this.stop = function() {
       cancelAnimationFrame(request);
     };
   }
 }
-
