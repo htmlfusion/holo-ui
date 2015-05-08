3a7992d45dbf132addb887a798f75ef0935aa738
Jesse Vander Does <jesse@htmlfusion.com>
2015-05-07 17:59:43 -0700

    Sweet updates to drop box scene

--- web/js/DropBoxDemo.js
+++ web/js/DropBoxDemo.js
@@ -32,8 +32,55 @@ function DropBoxDemo(scene) {
     box.position.y = shadowBox.position.y;
     box.position.z = shadowBox.position.z;
 
+    shadowBox.material.color = 'black';
+
     scene.remove(shadowBox);
     scene.add(box);
+
+
+    // Floor
+    var floor = new Physijs.BoxMesh(
+      new THREE.BoxGeometry( 1000, 2, 1000, 0, 0, 0),
+      new THREE.MeshPhongMaterial({ color: 'blue' , transparent: true, opacity: 0}),
+      0
+    );
+
+    floor.position.y = -150;
+
+    scene.add(floor);
+
+    // Furniture 1
+    var tableB = new Physijs.BoxMesh(
+      new THREE.BoxGeometry( 55, 2, 55, 0, 0, 0),
+      shadowBox.material,
+      0
+    );
+    tableB.position.x = -10;
+    tableB.position.y = -35;
+    tableB.position.z = -200;
+    scene.add(tableB);
+
+    // Furniture 3
+    var tableC = new Physijs.BoxMesh(
+      new THREE.BoxGeometry( 140, 2, 55, 0, 0, 0),
+      shadowBox.material,
+      0
+    );
+    tableC.position.x = 50;
+    tableC.position.y = -85;
+    tableC.position.z = -200;
+    scene.add(tableC);
+
+    // Screen
+    var screen = new Physijs.BoxMesh(
+      new THREE.BoxGeometry( 70, 25, 2, 0, 0, 0),
+      shadowBox.material,
+      0
+    );
+    screen.position.x = -5;
+    screen.position.y = 0;
+    screen.position.z = -200;
+    scene.add(screen);
 
     // Materials
     var table_material = Physijs.createMaterial(
@@ -56,13 +103,38 @@ function DropBoxDemo(scene) {
     table.receiveShadow = true;
 
     table.position.x = tableBox.position.x;
-    table.position.y = tableBox.position.y;
+    table.position.y = tableBox.position.y-5;
     table.position.z = tableBox.position.z-8;
 
     table.rotation.x = -.03;
 
     scene.remove(tableBox);
     scene.add(table);
+
+    THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
+
+    var loader = new THREE.OBJMTLLoader();
+    loader.load('obj/Room.obj', '', function(object) {
+      //object.position.y = 0;
+      //object.position.z = 35;
+      //object.position.x = 0;
+
+      var wireframe = new THREE.MeshBasicMaterial({wireframe: true, color: 'grey'});
+
+      object.scale.set(-100, -100, 100);
+      object.rotation.set(0, 45*Math.PI/180, 0);
+      object.position.set(-120, 300, -750);
+
+      //scene.add(object);
+
+      console.log("Object added to scene");
+
+    }, function(progress){
+      console.log(progress);
+    }, function(err){
+      console.log(err);
+    });
+
 
     loaded = true;
   }
@@ -114,7 +186,7 @@ function DropBoxDemo(scene) {
 
       geo.position.x = getRandomArbitrary(-5, 5);
       geo.position.y = getRandomArbitrary(29, 50);
-      geo.position.z = getRandomArbitrary(-10, 100);
+      geo.position.z = getRandomArbitrary(0, -400);
       geo.rotation.x = Math.random();
       geo.rotation.y = Math.random();
       geo.rotation.z = Math.random();
