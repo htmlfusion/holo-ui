fda52f043698afe6ec4d7d8fbc2e9b8f82b7400b
Jesse Vander Does <jesse@htmlfusion.com>
2015-05-20 15:56:13 -0700

    Baaah, calibration as good as it will get

--- web/js/DropBoxDemo.js
+++ web/js/DropBoxDemo.js
@@ -2,8 +2,6 @@ function DropBoxDemo(scene) {
 
   var self = this;
   var scene = scene;
-  var shadowBox = scene.getObjectByName('shadowBox');
-  var tableBox = scene.getObjectByName('Table');
   var spotLight = scene.getObjectByName('lamp');
   var loaded = false;
 
@@ -12,161 +10,188 @@ function DropBoxDemo(scene) {
     spotLight.castShadow = true;
 
     //spotLight.shadowDarkness = 0.5;
-    spotLight.shadowMapWidth = 1024*8;
-    spotLight.shadowMapHeight = 1024*8;
+    spotLight.shadowMapWidth = 1024 * 8;
+    spotLight.shadowMapHeight = 1024 * 8;
     //spotLight.shadowCameraVisible = true;
     spotLight.position.x = 60;
     spotLight.shadowCameraNear = 5;
     spotLight.shadowCameraFov = 120;
     spotLight.target.position.set(0, 5, -50);
 
-    shadowBox.material.side = THREE.DoubleSide
+    // shadowBox.material.side = THREE.DoubleSide
 
-    var box_material = Physijs.createMaterial(
-      shadowBox.material
-      // 0.1,
-      // 1
-    );
+    // var box_material = Physijs.createMaterial(
+    //   shadowBox.material
+    //   // 0.1,
+    //   // 1
+    //);
 
-    var box = new Physijs.BoxMesh(
-      shadowBox.geometry,
-      box_material
-    );
+    // var box = new Physijs.BoxMesh(
+    //   shadowBox.geometry,
+    //   box_material
+    // );
 
-    box.position.x = shadowBox.position.x;
-    box.position.y = shadowBox.position.y;
-    box.position.z = shadowBox.position.z;
+    // box.position.x = shadowBox.position.x;
+    // box.position.y = shadowBox.position.y;
+    // box.position.z = shadowBox.position.z;
 
-    shadowBox.material.color = 'black';
+    // shadowBox.material.color = 'black';
 
-    scene.remove(shadowBox);
+    // scene.remove(shadowBox);
     //scene.add(box);
 
 
     // Floor
-    var floor = new Physijs.BoxMesh(
-      new THREE.BoxGeometry( 1000, 2, 1000, 0, 0, 0),
-      new THREE.MeshPhongMaterial({ color: 'blue' , transparent: true, opacity: 0}),
-      0
-    );
+    // var floor = new Physijs.BoxMesh(
+    //   new THREE.BoxGeometry( 1000, 2, 1000, 0, 0, 0),
+    //   new THREE.MeshPhongMaterial({ color: 'blue' , transparent: true, opacity: 0}),
+    //   0
+    // );
 
-    floor.position.y = -150;
+    //floor.position.y = -150;
 
-    scene.add(floor);
+    //scene.add(floor);
 
     // Furniture 1
-    var tableB = new Physijs.BoxMesh(
-      new THREE.BoxGeometry( 55, 50, 55, 0, 0, 0),
-      shadowBox.material,
-      0
-    );
-    tableB.position.x = -10;
-    tableB.position.y = -65;
-    tableB.position.z = -200;
-    scene.add(tableB);
+    // var tableB = new Physijs.BoxMesh(
+    //   new THREE.BoxGeometry( 55, 50, 55, 0, 0, 0),
+    //   shadowBox.material,
+    //   0
+    // );
+    // tableB.position.x = -10;
+    // tableB.position.y = -65;
+    // tableB.position.z = -200;
+    // scene.add(tableB);
 
-    // Furniture 3
-    var tableC = new Physijs.BoxMesh(
-      new THREE.BoxGeometry( 140, 2, 55, 0, 0, 0),
-      shadowBox.material,
-      0
-    );
-    tableC.position.x = 50;
-    tableC.position.y = -85;
-    tableC.position.z = -200;
-    scene.add(tableC);
-
-
-    //Cup
-    var cup = new Physijs.ConcaveMesh(
-      new THREE.CylinderGeometry(9.525/2, 9.525/2, 11.43, 20, 20, true, 0, Math.PI * 2),
-      new THREE.MeshPhongMaterial({ color: 'black' }),
-      0
-    );
-
-    cup.castShadow = true;
-    cup.position.set(3.7801897525787354, tableBox.position.y, -38.69133377075195);
-    cup.material.side = THREE.DoubleSide
-    scene.add(cup);
+    // // Furniture 3
+    // var tableC = new Physijs.BoxMesh(
+    //   new THREE.BoxGeometry( 140, 2, 55, 0, 0, 0),
+    //   shadowBox.material,
+    //   0
+    // );
+    // tableC.position.x = 50;
+    // tableC.position.y = -85;
+    // tableC.position.z = -200;
+    // scene.add(tableC);
 
 
-    // Screen
-    var screen = new Physijs.BoxMesh(
-      new THREE.BoxGeometry( 70, 45, 2, 0, 0, 0),
-      shadowBox.material,
-      0
-    );
-    screen.position.x = -5;
-    screen.position.y = 0;
-    screen.position.z = -200;
-    scene.add(screen);
+    // //Cup
+    // var cup = new Physijs.ConcaveMesh(
+    //   new THREE.CylinderGeometry(9.525/2, 9.525/2, 11.43, 20, 20, true, 0, Math.PI * 2),
+    //   new THREE.MeshPhongMaterial({ color: 'black' }),
+    //   0
+    // );
 
-    // Materials
-    var table_material = Physijs.createMaterial(
-      tableBox.material
-      // .8, // high friction
-      // .9 // low restitution
-    );
+    // cup.castShadow = true;
+    // cup.position.set(3.7801897525787354, tableBox.position.y, -38.69133377075195);
+    // cup.material.side = THREE.DoubleSide
+    // scene.add(cup);
 
-    // Ground
-    var table = new Physijs.BoxMesh(
-      tableBox.geometry,
-      table_material,
-      0 // mass
-    );
 
-    box.castShadow = true;
-    box.receiveShadow= true;
+    // // Screen
+    // var screen = new Physijs.BoxMesh(
+    //   new THREE.BoxGeometry( 70, 45, 2, 0, 0, 0),
+    //   shadowBox.material,
+    //   0
+    // );
+    // screen.position.x = -5;
+    // screen.position.y = 0;
+    // screen.position.z = -200;
+    // scene.add(screen);
 
-    table.castShadow = true;
-    table.receiveShadow = true;
+    // // Materials
+    // var table_material = Physijs.createMaterial(
+    //   tableBox.material
+    //   // .8, // high friction
+    //   // .9 // low restitution
+    // );
 
-    table.position.x = tableBox.position.x;
-    table.position.y = tableBox.position.y-5;
-    table.position.z = tableBox.position.z-8;
+    // // Ground
+    // var table = new Physijs.BoxMesh(
+    //   tableBox.geometry,
+    //   table_material,
+    //   0 // mass
+    // );
 
-    table.rotation.x = -.03;
+    // box.castShadow = true;
+    // box.receiveShadow= true;
 
-    scene.remove(tableBox);
-    scene.add(table);
+    // table.castShadow = true;
+    // table.receiveShadow = true;
+
+    // table.position.x = tableBox.position.x;
+    // table.position.y = tableBox.position.y-5;
+    // table.position.z = tableBox.position.z-8;
+
+    // table.rotation.x = -.03;
+
+    // scene.remove(tableBox);
+    // scene.add(table);
 
     THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
 
-    //var loader = new THREE.OBJMTLLoader();
-    //loader.load('obj/Room.obj', '', function(object) {
-    //  //object.position.y = 0;
-    //  //object.position.z = 35;
-    //  //object.position.x = 0;
-    //
-    //  var wireframe = new THREE.MeshBasicMaterial({wireframe: true, color: 'grey'});
-    //
-    //  object.scale.set(-100, -100, 100);
-    //  object.rotation.set(0, 45*Math.PI/180, 0);
-    //  object.position.set(-120, 300, -750);
-    //
-    //  //scene.add(object);
-    //
-    //  console.log("Object added to scene");
-    //
-    //}, function(progress){
-    //  console.log(progress);
-    //}, function(err){
-    //  console.log(err);
-    //});
+    var loader = new THREE.OBJMTLLoader();
+    loader.load('obj/full-room-modeled.obj', '', function(object) {
+      //object.position.y = 0;
+      //object.position.z = 35;
+      //object.position.x = 0;
+
+      var wireframe = new THREE.MeshBasicMaterial({
+        wireframe: true,
+        color: 'grey'
+      });
+
+      // object.scale.set(-100, -100, 100);
+      // object.position.set(-120, 300, -750);
+
+      var children = object.children;
+      object.children = [];
+      children.forEach(function(child) {
+
+        var rigidMaterial = Physijs.createMaterial(
+          wireframe,
+          .8,
+          .3
+        );
+
+        // Ground
+        var childObject = new Physijs.ConvexMesh(
+          child.geometry,
+          rigidMaterial,
+          0 // mass
+        );
+
+        object.add(childObject);
+        //scene.add(childObject);
+      });
+
+      object.rotation.set(-2*Math.PI/180, -95 * Math.PI / 180, 0);
+      object.position.y = 5;
+      scene.add(object);
+
+      console.log("Object added to scene");
+
+    }, function(progress) {
+      console.log(progress);
+    }, function(err) {
+      console.log(err);
+    });
 
 
     leapFrame = 0;
-    Leap.loop( function(frame) {
-      frame.hands.forEach(function (hand, index) {
-        if(hand.grabStrength === 0 && leapFrame % 12 === 0){
+    Leap.loop(function(frame) {
+      frame.hands.forEach(function(hand, index) {
+        if (hand.grabStrength === 0 && leapFrame % 12 === 0) {
           var geo = new Physijs.SphereMesh(
-            new THREE.SphereGeometry( 4, 40, 40 ),
-            new THREE.MeshPhongMaterial({ color: 'yellow' })
+            new THREE.SphereGeometry(4, 40, 40),
+            new THREE.MeshPhongMaterial({
+              color: 'yellow'
+            })
           );
           geo.receiveShadow = true;
-          geo.position.set(hand.palmPosition[0]/10-10, hand.palmPosition[1]/10 - 30, hand.palmPosition[2]/10 + 25);
+          geo.position.set(hand.palmPosition[0] / 10 - 10, hand.palmPosition[1] / 10 - 30, hand.palmPosition[2] / 10 + 25);
           scene.add(geo);
-          geo.setLinearVelocity(new THREE.Vector3( 0, 0, -50));
+          geo.setLinearVelocity(new THREE.Vector3(0, 0, -50));
           leapFrame = 0;
         }
       });
@@ -182,20 +207,21 @@ function DropBoxDemo(scene) {
     this.load();
   }
 
-  var frame  = 0;
+  var frame = 0;
+
   function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
   }
 
-  this.animate = function(time){
+  this.animate = function(time) {
 
-    if(!loaded){
+    if (!loaded) {
       return;
     }
 
     frame++;
 
-    if(frame%30===0){
+    if (frame % 30 === 0) {
 
       var shape = Math.random(),
         size = Math.round(getRandomArbitrary(2, 8)),
@@ -209,15 +235,19 @@ function DropBoxDemo(scene) {
 
       color = colors[rand];
 
-      if(shape < .5){
+      if (shape < .5) {
         geo = new Physijs.BoxMesh(
-          new THREE.BoxGeometry( size, size, size , 10, 10, 10),
-          new THREE.MeshPhongMaterial({ color: color })
+          new THREE.BoxGeometry(size, size, size, 10, 10, 10),
+          new THREE.MeshPhongMaterial({
+            color: color
+          })
         );
       } else {
         geo = new Physijs.SphereMesh(
-          new THREE.SphereGeometry( size, 40, 40 ),
-          new THREE.MeshPhongMaterial({ color: color })
+          new THREE.SphereGeometry(size, 40, 40),
+          new THREE.MeshPhongMaterial({
+            color: color
+          })
         );
       }
 
@@ -229,12 +259,12 @@ function DropBoxDemo(scene) {
       geo.rotation.y = Math.random();
       geo.rotation.z = Math.random();
       geo.castShadow = true;
-      geo.receiveShadow= true;
+      geo.receiveShadow = true;
       geo.setCcdMotionThreshold(1);
-      geo.setCcdSweptSphereRadius(size/5);
+      geo.setCcdSweptSphereRadius(size / 5);
 
 
-      scene.add( geo );
+      scene.add(geo);
 
     }
 
